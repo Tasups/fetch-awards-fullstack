093dcf570b41da10b2b841944cb8e4ab3e3b750d
Jason Whisnant <tasups@gmail.com>
2022-05-18 20:46:31 +0000

    controllers started, app adjusted

--- app.js
+++ app.js
@@ -8,108 +8,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-let totalPoints = {};
-let distributedPoints = [];
-
-app.post('/deductPoints',(req,res)=>{
-    var sum = 0;
-    var reqpoints = req.body.points;
-    for(var val in totalPoints)
-    {
-        sum += totalPoints[val];
-    }
-    //check if the points requested to be deducted is lesser than the available points
-    if(sum < reqpoints)
-    {
-        res.send({"Message":"Available points is lesser than the deduction request"});
-    }
-
-    else
-    {
-        //sorting in descending order
-        distributedPoints = distributedPoints.sort((a,b)=> { return new Date(a.time) - new Date(b.time) });
-        let remaining = reqpoints;
-        let deductedJSON = {};
-        //calculating deductable points
-        for(var i = 0; i < distributedPoints.length; i++)
-        {
-            var deducted = 0;
-            let cp = distributedPoints[i];
-            if(cp.points - remaining >= 0)
-            {
-                deducted = remaining;
-                distributedPoints[i].points -= deducted;
-            }
-            else
-            {
-                deducted = cp.points;
-                distributedPoints[i].points = 0;
-            }
-            deducted *= parseInt(-1);
-            remaining += deducted;
-            if(deductedJSON[cp.payer])
-                deductedJSON[cp.payer] += deducted;
-            else
-                deductedJSON[cp.payer] = deducted;
-
-            if(remaining <= 0)
-            {
-                //resetting totalPoints to 0 to calculate fresh
-                for(var k in totalPoints)
-                {
-                    totalPoints[k] = 0;
-                }
-
-                //removing the 0 points used up user entries
-                for(var j = 0; j<distributedPoints.length ; j++)
-                {
-                    if(distributedPoints[j].points == 0)
-                    {
-                        distributedPoints.splice(j,1);
-                        j--;
-                    }
-                    else
-                    {
-                        if(totalPoints[distributedPoints[j].payer])
-                            totalPoints[distributedPoints[j].payer] += distributedPoints[j].points;
-                        else
-                            totalPoints[distributedPoints[j].payer] = distributedPoints[j].points;
-                    }
-                }
-                //sending response and breaking out
-                res.send(deductedJSON);
-                break;
-            }
-
-        }
-    }
-
-});
-
-app.post('/addPoints', (req, res) => {
-    //pushing into ditributedPoints and totaling the points in totalPoints variables
-    let record = req.body;
-    record.time = moment(record.time,"MM/DD/YYYY h:m a").toDate();
-
-    distributedPoints.push(record);
-
-        if(totalPoints[record.payer])
-        {
-            totalPoints[record.payer] += record.points;
-        }
-        else
-        {
-            totalPoints[record.payer] = record.points;
-        }
-
-    console.log(distributedPoints);
-    res.send(totalPoints);  
-});
+const points = require('./routes/points')
 
 
-app.get('/getPoints',(req,res) => {
-    res.send(totalPoints);
-});
+app.use(express.json())
+
+app.use('api/v1/points', points)
 
 
 const port = process.env.PORT || 3001; 
@@ -125,3 +29,107 @@ const start = async () => {
 }
 
 start();
+
+
+// let totalPoints = {};
+// let distributedPoints = [];
+
+// app.post('/deductPoints',(req,res)=>{
+//     var sum = 0;
+//     var reqpoints = req.body.points;
+//     for(var val in totalPoints)
+//     {
+//         sum += totalPoints[val];
+//     }
+//     //check if the points requested to be deducted is lesser than the available points
+//     if(sum < reqpoints)
+//     {
+//         res.send({"Message":"Available points is lesser than the deduction request"});
+//     }
+
+//     else
+//     {
+//         //sorting in descending order
+//         distributedPoints = distributedPoints.sort((a,b)=> { return new Date(a.time) - new Date(b.time) });
+//         let remaining = reqpoints;
+//         let deductedJSON = {};
+//         //calculating deductable points
+//         for(var i = 0; i < distributedPoints.length; i++)
+//         {
+//             var deducted = 0;
+//             let cp = distributedPoints[i];
+//             if(cp.points - remaining >= 0)
+//             {
+//                 deducted = remaining;
+//                 distributedPoints[i].points -= deducted;
+//             }
+//             else
+//             {
+//                 deducted = cp.points;
+//                 distributedPoints[i].points = 0;
+//             }
+//             deducted *= parseInt(-1);
+//             remaining += deducted;
+//             if(deductedJSON[cp.payer])
+//                 deductedJSON[cp.payer] += deducted;
+//             else
+//                 deductedJSON[cp.payer] = deducted;
+
+//             if(remaining <= 0)
+//             {
+//                 //resetting totalPoints to 0 to calculate fresh
+//                 for(var k in totalPoints)
+//                 {
+//                     totalPoints[k] = 0;
+//                 }
+
+//                 //removing the 0 points used up user entries
+//                 for(var j = 0; j<distributedPoints.length ; j++)
+//                 {
+//                     if(distributedPoints[j].points == 0)
+//                     {
+//                         distributedPoints.splice(j,1);
+//                         j--;
+//                     }
+//                     else
+//                     {
+//                         if(totalPoints[distributedPoints[j].payer])
+//                             totalPoints[distributedPoints[j].payer] += distributedPoints[j].points;
+//                         else
+//                             totalPoints[distributedPoints[j].payer] = distributedPoints[j].points;
+//                     }
+//                 }
+//                 //sending response and breaking out
+//                 res.send(deductedJSON);
+//                 break;
+//             }
+
+//         }
+//     }
+
+// });
+
+// app.post('/addPoints', (req, res) => {
+//     //pushing into ditributedPoints and totaling the points in totalPoints variables
+//     let record = req.body;
+//     record.time = moment(record.time,"MM/DD/YYYY h:m a").toDate();
+
+//     distributedPoints.push(record);
+
+//         if(totalPoints[record.payer])
+//         {
+//             totalPoints[record.payer] += record.points;
+//         }
+//         else
+//         {
+//             totalPoints[record.payer] = record.points;
+//         }
+
+//     console.log(distributedPoints);
+//     res.send(totalPoints);  
+// });
+
+
+// app.get('/getPoints',(req,res) => {
+//     res.send(totalPoints);
+// });
---
+++ controllers/points.js
@@ -0,0 +1,26 @@
+const Points = require('../models/Points');
+const asyncWrapper = require('../middleware/async');
+
+/* GET ALL POINTS BY PAYER THIS ENDPOINT NEEDS TO CHANGE TO LOOK AT ALL THE OBJECTS IN THE DATABASE AND THEN GROUP THEM ALTOGETHER BY PAYER */
+
+const getPoints = asyncWrapper(async (req, res) => {
+  const points = await Points.find({}) //this gets all objects from the db now to just filter through them and gather the payers and add the points together
+  res.status(200).json({ points })
+})
+
+/* ADD POINTS TO THE DATABASE */
+
+const addPoints = asyncWrapper(async (req, res) => {
+  const points = await Points.create(req.body)
+  res.status(201).json({ points })
+})
+
+const spendPoints = asyncWrapper(async (req, res) => {
+  const points = await Points.find({}) //this gets all objects from the db now to just filter through them for the oldest points first
+})
+
+module.exports = {
+  getPoints,
+  addPoints,
+  spendPoints
+}
