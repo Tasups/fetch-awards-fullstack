508eece83adf3d0186f7811ef8f277cfb50688e9
Jason Whisnant <tasups@gmail.com>
2022-05-25 20:45:28 +0000

    need to find the difference to finish this up

--- algoTest.js
+++ algoTest.js
@@ -30,21 +30,22 @@ const points = [
 
 let pointsPayment = 5000;
 
-const paymentProcess = (array) => {
-  // store initial array so that the difference can be calculated later and returned
-  let pointsArrayClone = []
-  for (let j = 0; j < array.length; j++) {
+// store initial array so that the difference can be calculated later and returned
+let pointsArrayClone = []
+  for (let j = 0; j < points.length; j++) {
     let jObj = {}
-    jObj['payer'] = array[j].payer
-    jObj['points'] = array[j].points
+    jObj['payer'] = points[j].payer
+    jObj['points'] = points[j].points
     pointsArrayClone.push(jObj)
   }
 
+const paymentProcess = (array) => {
+  
   // as long as pointsPayment hasn't been paid down/isn't 0 do this
   while (pointsPayment > 0) {
     // iterate through the points array
     for (let i = 0; i < array.length; i++) {
-      console.log(array[i].payer, array[i].points)
+      // console.log(array[i].payer, array[i].points)
       // as long as the points to be spent are larger than the points in the array index [i], continue the process
       if (pointsPayment >= points[i].points) {
         // store the difference betweeen the points to be paid and the points in the array index[i]
@@ -53,19 +54,50 @@ const paymentProcess = (array) => {
         if (placeHolder >= 0) {
           points[i].points = 0
           pointsPayment = placeHolder
-          console.log(pointsPayment, points[i].payer, points[i].points)
+          //console.log(pointsPayment, points[i].payer, points[i].points)
         } 
         // if points stored are more than the points to be spent, subtract them from the points in the array index[0] and set placeHolder to 0, which later dictates what pointsPayment will be
       } else if (points[i].points > pointsPayment) {
           points[i].points -= pointsPayment
           pointsPayment = 0
-          console.log(points[i].payer, points[i].points)
+          //console.log(points[i].payer, points[i].points)
         } 
     }
     return pointsPayment
+    // now its time to use this method to get the difference between the two https://stackoverflow.com/questions/51013088/get-the-difference-two-objects-by-subtracting-properties
     // use mongodb call to list out each payer and their aggregate points change
   } 
+  
 }
 
 paymentProcess(points)
-console.log(pointsPayment, points)
+
+// can't remember why i did this? please help!!! 
+// use these websites for clarity: 
+// https://stackoverflow.com/questions/455338/how-do-i-check-if-an-object-has-a-key-in-javascript
+//https://stackoverflow.com/questions/8217419/how-to-determine-if-javascript-array-contains-an-object-with-an-attribute-that-e
+// https://bobbyhadz.com/blog/javascript-add-key-value-pair-to-object
+const pointsSpentByPayer = (points, pointsArrayClone) => {
+  let resultArray = []
+  let payerObj = {}
+  for (let i = 0; i < points.length; i++) {
+    
+  let objPlaceholder = { payer: points[i].payer, points: points[i].points }
+
+    resultArray.hasOwnProperty(points[i].payer) 
+      ? console.log('donkey')// add points to the points key where it has this same property
+      : resultArray.push(objPlaceholder)
+    
+    if (!payerObj.hasOwnProperty(points[i].payer)) {
+      payerObj['payer'] = points[i].payer;
+    }
+  }
+}
+
+
+
+console.log(pointsPayment, points, pointsArrayClone)
+
+if (pointsPayment === 0) {
+    console.log("YOU ARE OUT OF POINTS MOTHERFUCKER!!!")
+  }
--- codeTest.js
+++ codeTest.js
@@ -1,12 +1,23 @@
-var arr = [];
-var fruits = ['banana', 'apple', 'mango'];
-var label = 'Fruits';
+// var arr = [];
+// var fruits = ['banana', 'apple', 'mango'];
+// var label = 'Fruits';
 
-for(var i = 0; i < fruits.length; i++) {
-    var obj = {};
-    obj['data'] = fruits[i];
-    obj['label'] = label;
-    arr.push(obj);
-}
+// for(var i = 0; i < fruits.length; i++) {
+//     var obj = {};
+//     obj['data'] = fruits[i];
+//     obj['label'] = label;
+//     arr.push(obj);
+// }
 
-console.log(arr);
+// console.log(arr);
+
+let previousChart =  {BWP: 1, ZAR: 1.3, USD: 0.09324, number: 1};
+let currentChart = {BWP: 1.25, ZAR: 1.35, USD: 0.01, number: 2};
+
+let newObj = Object.keys(previousChart).reduce((a, k) => {
+    a[k] = previousChart[k] - currentChart[k];
+    return a;
+}, {});
+
+console.log(newObj);
+
